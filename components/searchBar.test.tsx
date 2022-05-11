8c27c04a15df7ddf62566f5c6f7fd9406adc5e0a
pankotaimarksandor <pankotaimarksandor@gmail.com>
2022-05-11 10:34:21 +0200

    search bar tests

---
+++ components/searchBar.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { store } from '../redux/store'
+import user from '@testing-library/user-event'
+import SearchBar from './searchBar'
+
+describe('<SearchBar />', () => {
+    it('should display an empty input field and a button', () => {
+        render(<SearchBar onSearchHandler={jest.fn()}/>)
+
+        const inputField = screen.getByRole('textbox')
+        expect(inputField).toBeInTheDocument()
+        expect(inputField).toBeEmpty()
+
+        const button = screen.getByText('Search')
+        expect(button).toBeInTheDocument()
+    })
+    it('should update text when user types', async () => {
+        render(<SearchBar onSearchHandler={jest.fn()}/>)
+
+        const inputField = screen.getByRole('textbox')
+
+        await user.clear(inputField)
+        await user.type(inputField, 'asdasd')
+
+        await expect(inputField).toHaveDisplayValue('asdasd')
+    })
+})
